b46133f0bb6cbc36a355e6bafe7253e7a3e70693
jarbasai <jarbasai@mailfence.com>
2017-09-21 01:08:51 +0100

    test

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from synthesizer import Synthesizer
-from os.path import dirname
+from os.path import dirname, exists
+from os import mkdir
 import time
 
 model = "tacotron-20170720"
@@ -11,9 +12,12 @@ synthesizer.load(path)
 texts = ["bitcoin is the future",
          "long live the blockchain",
          "i love artificial intelligence",
-         "my name is Jarbas and i am an open source artificial intelligenc"]
+         "my name is Jarbas and i am an open source artificial intelligence"]
 
 base_out = dirname(__file__) + "/output_samples/"
+if not exists(base_out):
+    mkdir(base_out)
+
 for text in texts:
     start = time.time()
     out = base_out + text.replace(" ", "_") + ".wav"
